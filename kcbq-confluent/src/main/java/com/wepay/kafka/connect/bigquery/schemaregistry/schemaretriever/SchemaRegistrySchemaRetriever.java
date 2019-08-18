205b9fc6f522003399e3c7b564d5e2e7afd10aa7
Ian Duffy <ian@ianduffy.ie>
2019-08-18 01:19:06 +0100

    Downgrade schema registry client

--- kcbq-confluent/src/main/java/com/wepay/kafka/connect/bigquery/schemaregistry/schemaretriever/SchemaRegistrySchemaRetriever.java
+++ kcbq-confluent/src/main/java/com/wepay/kafka/connect/bigquery/schemaregistry/schemaretriever/SchemaRegistrySchemaRetriever.java
@@ -50,12 +50,9 @@ public class SchemaRegistrySchemaRetriever implements SchemaRetriever {
   public void configure(Map<String, String> properties) {
     SchemaRegistrySchemaRetrieverConfig config =
         new SchemaRegistrySchemaRetrieverConfig(properties);
-    Map<String, ?> schemaRegistryClientProperties =
-        config.originalsWithPrefix(config.SCHEMA_REGISTRY_CLIENT_PREFIX);
     schemaRegistryClient = new CachedSchemaRegistryClient(
         config.getString(config.LOCATION_CONFIG),
-        0,
-        schemaRegistryClientProperties
+        0
     );
     avroData = new AvroData(config.getInt(config.AVRO_DATA_CACHE_SIZE_CONFIG));
   }
--- kcbq-confluent/src/test/java/com/wepay/kafka/connect/bigquery/schemaregistry/schemaretriever/SchemaRegistrySchemaRetrieverConfigTest.java
+++ kcbq-confluent/src/test/java/com/wepay/kafka/connect/bigquery/schemaregistry/schemaretriever/SchemaRegistrySchemaRetrieverConfigTest.java
@@ -1,6 +1,5 @@
 package com.wepay.kafka.connect.bigquery.schemaregistry.schemaretriever;
 
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -14,9 +13,6 @@ public class SchemaRegistrySchemaRetrieverConfigTest {
   public void testClientPrefix() {
     final Map<String, String> clientProperties = new HashMap<>();
     final Map<String, String> allProperties = new HashMap<>();
-
-    clientProperties.put(SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "SASL_INHERIT");
-    clientProperties.put(SchemaRegistryClientConfig.USER_INFO_CONFIG, "foo:bar");
 
     allProperties.put(SchemaRegistrySchemaRetrieverConfig.AVRO_DATA_CACHE_SIZE_CONFIG, "69");
     allProperties.put(SchemaRegistrySchemaRetrieverConfig.LOCATION_CONFIG, "http://localhost:8083");
